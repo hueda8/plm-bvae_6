1a50f5babb4f5f1bdc4ca79e71fae8cf6c6dd057
Hiromi UEDA <ueda.hiromi2@kitasato-u.ac.jp>
2026-04-07 12:12:01 +0900

    Update print statement from 'Hello' to 'Goodbye'

--- train.py
+++ train.py
@@ -528,9 +528,6 @@ def run_epoch(split: str,
     for batch in tqdm(gen(hp, split), desc=f"{split} epoch {epoch}", leave=False):
         if rt['use_precomputed']:
             batch_x, batch_y = batch
-            # 検証
-            print("batch_x[0].shape[0]", batch_x[0].shape[0], flush=True)
-            print("np.sum(batch_y[0] != 0) - 1", np.sum(batch_y[0] != 0) - 1, flush=True)
             x = torch.from_numpy(batch_x).float().to(device)
             select_indices_batch = None
         else:
